a1a315a6e977bb8b48386cc799ddfba8e2958ea4
Evgenii de-Bur <evd8@inbox.ru>
2024-12-09 01:16:34 +0300

    Версия 4. Ходы короля и коня

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ const container = document.getElementById("container");
 let arr_chessboard = [];
 console.log(arr_chessboard);
 
+const blackKing = {
+  string: 0,
+  row: 3,
+};
+const blackKnight = {
+  string: 0,
+  row: 1,
+};
+
 const pawn_black = document.getElementById("pawn_black");
 const bishop_white = document.getElementById("bishop_white");
 const knight_white = document.getElementById("knight_white");
@@ -17,7 +26,7 @@ theme.addEventListener("change", function () {
 
   if (theme.checked) {
     field_black.forEach((field) => {
-      field.style.background = "black";
+      field.style.background = "darkgreen";
     });
     field_white.forEach((field) => {
       field.style.background = "grey";
@@ -32,40 +41,65 @@ theme.addEventListener("change", function () {
   }
 });
 
-for (let i = 0; i < 8; i++) {
-  arr_chessboard.push([]);
-  let row = document.createElement("div");
-  row.className = "row";
+document.querySelector(".btn_King").onclick = MoveKing;
+document.querySelector(".btn_Knight").onclick = MoveKnight;
 
-  for (let j = 0; j < 8; j++) {
-    const field = document.createElement("div");
-    field.className = "field";
-    field.id = (i + j) % 2 === 0 ? "field_white" : "field_black";
-    if (i === 1 && j === 3) {
-      field.appendChild(pawn_black);
-    }
-    if (i === 0 && j === 3) {
-      field.appendChild(king_black);
-    }
-    if (i === 0 && j === 3) {
-      field.appendChild(king_black);
-    }
-
-    if (i === 7 && j === 4) {
-      field.appendChild(king_white);
-    }
-    if (i === 7 && j === 2) {
-      field.appendChild(bishop_white);
-    }
-    if (i === 7 && j === 6) {
-      field.appendChild(knight_white);
-    }
-    if (i === 0 && j === 1) {
-      field.appendChild(knight_black);
-    }
-
-    arr_chessboard[i].push(j);
-    row.appendChild(field);
-  }
-  container.appendChild(row);
+function MoveKing() {
+  arr_chessboard = [];
+  container.replaceChildren();
+  blackKing.string++;
+  FieldDrawing();
 }
+function MoveKnight() {
+  arr_chessboard = [];
+  container.replaceChildren();
+  blackKnight.string = blackKnight.string + 2;
+  blackKnight.row++;
+
+  FieldDrawing();
+}
+
+function FieldDrawing() {
+  arr_chessboard = [];
+  for (let i = 0; i < 8; i++) {
+    arr_chessboard.push([]);
+    let row = document.createElement("div");
+    row.className = "row";
+    // container.removeChild(row);
+
+    for (let j = 0; j < 8; j++) {
+      const field = document.createElement("div");
+      field.className = "field";
+      field.id = (i + j) % 2 === 0 ? "field_white" : "field_black";
+
+      if (i === blackKing.string && j === blackKing.row) {
+        field.appendChild(king_black);
+      }
+      if (i === blackKnight.string && j === blackKnight.row) {
+        field.appendChild(knight_black);
+      }
+      // if (i === 1 && j === 3) {
+      //   field.appendChild(pawn_black);
+      // }
+
+      // if (i === 7 && j === 4) {
+      //   field.appendChild(king_white);
+      // }
+      // if (i === 7 && j === 2) {
+      //   field.appendChild(bishop_white);
+      // }
+      // if (i === 7 && j === 6) {
+      //   field.appendChild(knight_white);
+      // }
+      // if (i === 0 && j === 1) {
+      //   field.appendChild(knight_black);
+      // }
+
+      arr_chessboard[i].push(j);
+      row.appendChild(field);
+    }
+    container.appendChild(row);
+  }
+}
+
+FieldDrawing();
